5e8a955225f84bce18c3c508798c031b70491a4c
2200515 <2200515@systex.com>
2023-11-28 11:03:44 +0800

    「商品功能 - 實作刪除商品」

--- src/main/java/com/yuchi/springbootmall/controller/ProductController.java
+++ src/main/java/com/yuchi/springbootmall/controller/ProductController.java
@@ -54,4 +54,14 @@ public class ProductController {
 
 
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+
+
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
 }
