43e261b4092eb7edb24000bd1bd7a2a07b74483a
2200515 <2200515@systex.com>
2023-11-30 11:12:46 +0800

    帳號功能 - 使用 MD5 加密密碼

--- src/main/java/com/yuchi/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/yuchi/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -28,7 +29,9 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-
+        // 使用 MD5 生成密碼雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
         //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -42,17 +45,21 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
 
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
-
+        //檢查 EMAIL 是否註冊
         if (user == null){
             log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-
-        if(userLoginRequest.getPassword().equals(user.getPassword())){
+        //使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+        //資料庫創建的加密密碼 比對 前端傳過來的加密密碼是否一致
+        if(user.getPassword().equals(hashedPassword)){
             return user;
 
         }else{
             log.warn("該email {} 密碼不正確", userLoginRequest.getEmail());
+            log.warn("輸入的密碼 {} ",userLoginRequest.getPassword());
+            log.warn("資料庫的密碼 {}", hashedPassword);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
     }
